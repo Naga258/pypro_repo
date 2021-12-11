c6c2a08cbb93f6489db26d3c188428e722840447
Naga258 <nagababu.thota258@gmail.com>
2021-12-11 16:55:45 +0530

    added project initial files

--- demo.py
+++ demo.py
@@ -1,3 +1,22 @@
-lst = [1,2,3,4]
-print(lst)
-print("Hello Git")
+# lst = [1,2,3,4]
+# print(lst)
+# print("Hello Git")
+
+# totalitems = int(float(input ( "How many items would you like ? ")))
+# print(totalitems)
+
+# rooms = {1:'Foyer',2:'Conference room'}
+# room = int(input ('Enter the room number : '))
+# if  not room  in rooms:
+#       #print("Match")
+#        print ('Room does not exists')
+# else:
+#       print("The room is " + rooms[room])
+
+
+# x1=2.5
+# y1=1
+# a = x1 / y1
+# print(type(a))
+
+
