37d22f76504367bb8ba54fb74c387c94819bdabb
Naga258 <nagababu.thota258@gmail.com>
2021-12-12 14:38:03 +0530

    added project initial files

--- demo.py
+++ demo.py
@@ -19,4 +19,65 @@
 # a = x1 / y1
 # print(type(a))
 
+# text = "This is text"
+# print(text)
+# letter = input("Enter the letter which you want to count : ")
+# cnt = 0
+# for word in text :
+#     #print(word)
+#     if word in letter :
+#         #print(word)
+#         cnt = cnt+1
+#     else :
+#         continue
+# print(cnt)
 
+
+# numlist  = [1,2,3,4,5]
+# alphalist = ['a','b','c','d','e']
+# print(numlist == alphalist)
+# numlist = alphalist
+# print(numlist  is alphalist)
+# print(numlist  == alphalist)
+
+# a=11
+# b=4
+# print(a/b)
+# print(a//b)
+# print(a%b)
+import math
+
+# print(math.fmod(1.5,2))
+# print(math.floor(1.5))
+# import random
+# print(random.randrange(5, 12, 1))
+# print(random.randint(5, 11))
+
+#
+# print("what is your name")
+# name = (input("name : "))
+# print(name)
+# print(type(name))
+
+# p=2
+# while p <= 100 :
+#     is_prime = True
+#     for i in range(2,p):
+#         #print(i)
+#         #print(is_prime)
+#         if p % i ==0 :
+#             is_prime = False
+#             break
+#         elif is_prime == True:
+#             print(p)
+#     p = p+1
+
+
+# productlist = [0,1,2,3,4,5,6,7,8,9]
+# index = 0
+# while index < 10 :
+#     print (productlist[index])
+#     if productlist[index] == 6 :
+#         break
+#     else :
+#         index = index +1
