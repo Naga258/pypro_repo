b1d455c831594eaa16efa649df12fca82168b7e1
Naga258 <nagababu.thota258@gmail.com>
2021-12-12 21:39:41 +0530

    added project initial files

--- demo.py
+++ demo.py
@@ -81,3 +81,19 @@ import math
 #         break
 #     else :
 #         index = index +1
+
+
+import sys
+try:
+    file_in = open("in.txt",'r')
+    file_out = open("out.txt", 'w+')
+except IOError:
+    print('cannot open')
+else :
+    i=1
+    for line in file_in:
+        print(line.rstrip())
+        file_out.write("line" + str(i) + ": " + line)
+        i = i+1
+    file_in.close()
+    file_out.close()
